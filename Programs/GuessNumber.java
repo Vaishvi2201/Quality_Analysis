0724903dfa61628e123e9aa67cf58613accad2bb
Vaishvi2201 <41586572+Vaishvi2201@users.noreply.github.com>
2021-02-12 17:27:21 +0530

    Update GuessNumber.java
    updated

--- Programs/GuessNumber.java
+++ Programs/GuessNumber.java
@@ -1,3 +1,4 @@
+
 /* Aim: To take the user input and check it matches with the jvm input
  * Allow the user to take 10 trials if not done try again should be used
  * Created on:8-02-2021
@@ -16,35 +17,32 @@ public class GuessNumber {
 				int jvm_input=rn.nextInt(100);
 				Scanner keyboard= new Scanner(System.in);
 
-				System.out.println("Enter an integer between 0 to 100 for guessing the correct number");
+				System.out.println("Enter two integer between 0 to 100 for guessing the correct number");
 				
 				for(int i=1;i<=10;i++) {
-					if(i<10) {
+					
+						Scanner sc1= new Scanner(System.in);
+						int user_input1= sc1.nextInt();
 						
-						Scanner sc= new Scanner(System.in);
-						int user_input= sc.nextInt();
+						Scanner sc2= new Scanner(System.in);
+						int user_input2= sc2.nextInt();
+						
+						int user_input = user_input1 + user_input2;
+						System.out.println("Sum=" +user_input);
+						
 						if(user_input==jvm_input) {
 							System.out.println("You guessed correct number"+" try no "+i);
 							break;
 						}	
-						else  {
-						
-					
-						if(jvm_input>user_input) {
+						else  if(jvm_input>user_input) {
 							System.out.println("The number is greater than what you guessed"+" try no "+i);
 						}
 					
 						else {
 							System.out.println("The number is less than what you guessed"+" try no "+i);
 							}	
-						}
-					}
-			
-					else {
-						System.out.println("Oops your 10 trials are over");
-						}
-				}
-					
+			}
+				System.out.println("10 trials are over");
 					
 					System.out.println("Do you want to continue the game Y/N");
 					answer=keyboard.next();
