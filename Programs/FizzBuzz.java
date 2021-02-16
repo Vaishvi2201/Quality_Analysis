70b900c2b19291229e788d912181154b8938b9c9
Vaishvi2201 <41586572+Vaishvi2201@users.noreply.github.com>
2021-02-16 11:52:23 +0530

    update with functions

--- Programs/FizzBuzz.java
+++ Programs/FizzBuzz.java
@@ -2,6 +2,8 @@
  * print last name if number is divisible by 5
  * print first name + last name if number is divisible by both 3 and 5
  * Created on:8-02-2021
+ * Updated on:10-02-2021(version 2)do while loop
+ * Updated on:15-02-2021(version 3)add functions
  * Author:Vaishvi Patel
  */
 
@@ -10,43 +12,43 @@ import java.util.Scanner;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-	
-		String first_name="Vaishvi";
-		String last_name="Patel";
-		String answer;
-		Scanner keyboard= new Scanner(System.in);
-		
-		do {
-				System.out.println("Enter an integer number");
-				Scanner sc= new Scanner(System.in);
-				int input= sc.nextInt();
-		
 
-					if(input%3==0 && input%5==0) {
-						System.out.println(first_name+" "+last_name);
-			
-					}
-		
-		
-					else if(input%5==0) {
-						System.out.println(last_name);
-					}
-		
-		
-					else if(input%3==0) {
-						System.out.println(first_name);
-					}
-		
-					else {
-						System.out.println("Number not divisible by 3 or 5");
-					}
-		
-					System.out.println("Do you want to continue the game Y/N");
-					answer=keyboard.next();
+		String answer;
+		Scanner keyboard = new Scanner(System.in);
+
+		do {
+			System.out.println("Enter an integer number");
+			Scanner sc = new Scanner(System.in);
+			int input = sc.nextInt();
+
+			Check_conditions(input);
+			System.out.println("Do you want to continue the game Y/N");
+			answer = keyboard.next();
+		} while (answer.equalsIgnoreCase("Y"));
+
+	}
+
+	public static void Check_conditions(int input) {
+		String first_name = "Vaishvi";
+		String last_name = "Patel";
+		if (input % 3 == 0 && input % 5 == 0) {
+			System.out.println(first_name + " " + last_name);
+
 		}
-			while(answer.equalsIgnoreCase("Y"));
-		
+
+		else if (input % 5 == 0) {
+			System.out.println(last_name);
+		}
+
+		else if (input % 3 == 0) {
+			System.out.println(first_name);
+		}
+
+		else {
+			System.out.println("Number not divisible by 3 or 5");
+		}
 
 	}
 
 }
+
